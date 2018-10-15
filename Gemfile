f4dedc6d2262a9d3875ef20d08c6665ab99a1c83
rhys117 <rhys117@gmail.com>
2018-10-15 13:16:57 +1000

    better email regex

--- Gemfile
+++ Gemfile
@@ -4,7 +4,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 ruby '2.5.0'
 
-gem 'pry'
-
 # Specify your gem's dependencies in surveyor.gemspec
 gemspec
--- lib/surveyor/response.rb
+++ lib/surveyor/response.rb
@@ -1,6 +1,7 @@
 module Surveyor
   class Response
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = %r(\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])
+                           ?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z)x
 
     attr_reader :email, :answers
 
